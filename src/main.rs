6431d8847e0ca4697fe67eeb34d185ac0b04e6c0
Mateusz Moneta <mateuszm@fb.com>
2020-05-10 10:52:28 +0100

    Add initial command structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Simple script to parse and combine savings in multiple currencies")]
+struct SavingsCalc {
+    #[structopt(subcommand)]
+    cmd: Command
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Add data to our savings spreadsheet
+    Add {
+        /// Input csv file
+        #[structopt(parse(from_os_str))]
+        input: PathBuf,
+    },
+    /// Parse our shaving spreadsheet and display data
+    Show {
+        /// Input csv file
+        #[structopt(parse(from_os_str))]
+        input: PathBuf,
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let opt = SavingsCalc::from_args();
+    println!("{:?}", opt);
 }
